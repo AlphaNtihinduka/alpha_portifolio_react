8331b09a7757e73ac15c349819320ea426e79bce
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2023-01-02 02:17:04 +0300

    Creating the portfolio components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import './App.css';
+import {Routes,Route} from "react-router-dom";
+import About from './components/About';
+import Home from './components/Home';
+import Sidebar from './components/Sidebar';
+import Skills from './components/Skills';
+import Resume from './components/Resume';
+import MyWork from './components/MyWork';
 
 function App() {
   return (
-    <div className="App">
-     Hello react
+    <div className="app-container container-fluid">
+      <div className="row flex-nowrap">
+        
+        <Sidebar></Sidebar>
+        <Routes>
+          <Route path="/" element={ <Home/> } exact></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/skills" element={<Skills/>}></Route>
+          <Route path="/resume" element={<Resume/>}></Route>
+          <Route path="/mywork" element={<MyWork/>}></Route>
+        </Routes>
+
+      </div>
     </div>
   );
 }
