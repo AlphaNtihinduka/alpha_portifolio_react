c76fede024753c48f1d0bff7debad96b785ea565
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2023-01-17 13:50:01 +0300

    Add description to the projects

---
+++ src/components/projects/ProjectDescription.js
@@ -0,0 +1,14 @@
+const ProjectsDescription = [
+  'Take control of your finances with Budget-app, track your spending, identify areas to cut back, achieve financial stability. Ruby on Rails, HTML, CSS',
+
+  'Covid-tacker helps the community, health workers, and government officials to know the cases of covid-19 within the time period. React JS, HTML, CSS',
+
+  'Users can like, comment on and reserve thier favourite movies. This applicaction uses an involvement API to save user interactions data. Javascript, HTML, CSS',
+
+  'The girls need to come together to discuss. This website shows the speakers and events that will take place at the conference. JAVASCRIPT, HTML, CSS',
+
+  'The food recipe helps to know which food ingredients are available and be able to determine which you are supposed to buy from the market. Ruby on rails',
+
+  'The users can be able to see the company wifi subscription, have ability to contact the marketing team and make reservations. HTML, CSS',
+];
+export default ProjectsDescription;
