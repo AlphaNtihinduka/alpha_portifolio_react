aed1912305d69442eb3e8ab3b04bc798d0332937
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2023-01-19 00:57:39 +0300

    Fix the grammar errors.

--- src/components/projects/ProjectDescription.js
+++ src/components/projects/ProjectDescription.js
@@ -1,14 +1,14 @@
 const ProjectsDescription = [
-  'Take control of your finances with Budget-app, track your spending, identify areas to cut back, and achieve financial stability. Ruby on Rails, HTML, CSS.',
+  'Take control of your finances with Budget-app. Track your spending, identify areas to cut back, and achieve financial stability. Built with Ruby on Rails, HTML, and CSS.',
 
   'Covid-tacker helps the community, health workers, and government officials to know the cases of covid-19 within the time period. React JS, HTML, CSS.',
 
-  'Users can like, comment on and reserve their favorite movies. This application uses an involvement API to save user interaction data. Javascript, HTML, CSS.',
+  'Covid-tracker helps the community, health workers, and government officials to track the cases of COVID-19 within a specific time period. Built with React JS, HTML, and CSS.',
 
-  'The girls need to come together to discuss. This website shows the speakers and events that will take place at the conference. JAVASCRIPT, HTML, CSS.',
+  'The girls need to come together to discuss. This website showcases the speakers and events that will take place at the conference. Built with JavaScript, HTML, and CSS.',
 
-  'The website of the organization helps the donors to track the activities of the organization. They can also visit the social handles. React JS, Webpack.',
+  'The website of the organization helps donors track the activities of the organization. They can also visit the social media handles of organization. Built with React JS and Webpack.',
 
-  'The users can be able to see the wifi subscription of the company have the ability to contact the marketing team, and make reservations. HTML, CSS.',
+  'Users can view the wifi subscription of the company, have the ability to contact the marketing team, and make reservations. Built with HTML and CSS.',
 ];
 export default ProjectsDescription;
